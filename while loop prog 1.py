e93e40cce9afad29f31f0bda2a23d38866651cfc
Channaveereshmeti2004 <167081535+Channaveereshmeti2004@users.noreply.github.com>
2024-04-17 21:52:08 +0530

    Create while loop prog 1.py

---
+++ while loop prog 1.py
@@ -0,0 +1,23 @@
+#while loop
+print("last line get exiguted")
+
+i = 23
+while i < 25:
+    print("statement-1")
+    print("statement-2")
+    print("statement-3")
+    i  = i + 1
+print("1st line get exiguted")
+print("2nd line get exiguted")
+
+
+output:
+last line get exiguted
+statement-1
+statement-2
+statement-3
+statement-1
+statement-2
+statement-3
+1st line get exiguted
+2nd line get exiguted
