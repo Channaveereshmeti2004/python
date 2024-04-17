65d69cc1047596b4a9dd2c4c22b7865d4b3871cc
Channaveereshmeti2004 <167081535+Channaveereshmeti2004@users.noreply.github.com>
2024-04-17 21:52:59 +0530

    Create while loop prog 2.py

---
+++ while loop prog 2.py
@@ -0,0 +1,18 @@
+i = 55
+while i < 97:
+    i= i + 1
+    print(i)
+24>>
+   i = 1224
+while i > 995:
+    i= i - 1
+    print(i)
+
+
+or
+
+i = 0
+while i < 10:
+    i = i+1
+    j= i * 5
+    print(j)
